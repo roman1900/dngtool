15873f563719cbbf1f67ad4097e4fd45d6b86501
Matt Marschall <mmarschall@redmars.com.au>
2022-03-18 09:39:36 +1100

    Added StringUtils Class

---
+++ src/main/java/au/com/redmars/StringUtils.java
@@ -0,0 +1,37 @@
+package au.com.redmars;
+
+import java.util.Arrays;
+
+public class StringUtils {
+
+	public static String formatObjectArray(byte[] bytes,int limit) {
+        if (bytes.length > limit) {
+            return String.format("%s ... %s\r\n",
+            Arrays.toString(Arrays.copyOfRange(bytes,0,limit / 2)),
+            Arrays.toString(Arrays.copyOfRange(bytes,bytes.length-limit / 2,bytes.length)));   
+        } 
+        else {
+            return Arrays.toString(bytes);
+        }
+    }
+	public static String formatObjectArray(Object[] values,int limit) {
+        if (values.length > limit) {
+            return String.format("%s ... %s\r\n",
+            Arrays.toString(Arrays.copyOfRange(values,0,limit / 2)),
+            Arrays.toString(Arrays.copyOfRange(values,values.length-limit / 2,values.length)));   
+        } 
+        else {
+            return Arrays.toString(values);
+        }
+    }
+	public static String formatObjectArray(double[] values,int limit) {
+        if (values.length > limit) {
+            return String.format("%s ... %s\r\n",
+            Arrays.toString(Arrays.copyOfRange(values,0,limit / 2)),
+            Arrays.toString(Arrays.copyOfRange(values,values.length-limit / 2,values.length)));   
+        } 
+        else {
+            return Arrays.toString(values);
+        }
+    }
+}
