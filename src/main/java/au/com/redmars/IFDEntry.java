8a82d31118dd2a4c28abd75eabb01b24045de8d2
Matt Marschall <mmarschall@redmars.com.au>
2022-03-21 16:22:31 +1100

    Modified IFDEntry to be a Super class

--- src/main/java/au/com/redmars/IFDEntry.java
+++ src/main/java/au/com/redmars/IFDEntry.java
@@ -1,17 +1,23 @@
 package au.com.redmars;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 
 public class IFDEntry {
 
 	private TagIdentifier tagIdentifier;
 	private FieldType fieldType;
-	private Integer count;
-	private Integer byteSize;
+	protected ByteOrder byteOrder;
+	protected Integer	count;
+	protected Integer byteSize;
 	private Boolean isValue;
-	private Integer offset;
-	private byte[] 	value;
-
+	protected Integer	offset;
+	protected byte[]	value;
+	protected byte[]	rawDNGBytes;
+	protected Integer	byteLocation;
+	
+	
 
 	public TagIdentifier getTagIdentifier(){
 		return tagIdentifier;
@@ -29,20 +35,14 @@ public class IFDEntry {
 		this.fieldType = fieldType;
 	}
 
-	public Boolean getIsValue() {
+	protected Boolean getIsValue() {
 		return isValue;
-	}
-
-	public byte[] getValue() {
-		if (isValue)
-			return value;
-		else 
-			return null;
 	}
 
 	public Integer getCount() {
 		return count;
 	}
+
 	public Integer getOffset() {
 		if (isValue)
 			return null;
@@ -54,19 +54,22 @@ public class IFDEntry {
 		return String.format("%-25s\t%-10s\tCount: %5d Value/s: ",tagIdentifier.name(),fieldType.name(),count);
 	}
 
-	public IFDEntry(byte[] rawTag,ImageHeader imageHeader) throws Exception{
-		int tag = imageHeader.getInt(Arrays.copyOfRange(rawTag, 0, 2));
+	public IFDEntry(byte[] rawDNGBytes,Integer byteLocation,ByteOrder byteOrder) throws Exception{
+		this.rawDNGBytes = rawDNGBytes;
+		this.byteOrder = byteOrder;
+		this.byteLocation = byteLocation;
+		byte[] rawTag = Arrays.copyOfRange(rawDNGBytes, byteLocation, byteLocation+12);
+		int tag = getInt(Arrays.copyOfRange(rawTag, 0, 2));
         if(TagIdentifier.valueOfTag(tag)!=null)
             tagIdentifier = TagIdentifier.valueOfTag(tag);
         else throw new Exception(String.format("Unknown TAG encountered: %d",tag));
-        
-		int type = imageHeader.getInt(Arrays.copyOfRange(rawTag, 2 , 4));
+
+		int type = getInt(Arrays.copyOfRange(rawTag, 2 , 4));
         if(FieldType.valueOfTag(type)!=null)
 			fieldType = FieldType.valueOfTag(type);
         else throw new Exception(String.format("Unknown Type encountered: %d",type));
-        
-		count = imageHeader.getLong(Arrays.copyOfRange(rawTag, 4 , 8)).intValue();
-        
+
+		count = getLong(Arrays.copyOfRange(rawTag, 4 , 8)).intValue();
 
 		switch (fieldType) {
 			case BYTE:
@@ -96,9 +99,26 @@ public class IFDEntry {
 
         if (isValue) 
 			value = Arrays.copyOfRange(rawTag, 8 , 12);
-        else
-            offset =  imageHeader.getLong(Arrays.copyOfRange(rawTag, 8 , 12)).intValue();
+        else {
+            offset =  getLong(Arrays.copyOfRange(rawTag, 8 , 12)).intValue();
+			value = Arrays.copyOfRange(rawDNGBytes, offset, offset + byteSize * count);
+		}
                 
 	}
-
+	
+	public Long getLong(byte[] bytes) {
+        return ((Integer)ByteBuffer.wrap(bytes).order(byteOrder).getInt()).longValue();
+    }
+    public int getInt(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).order(byteOrder).getChar();
+    }
+    public float getFloat(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).order(byteOrder).getFloat();
+    }
+	public Double getDouble(byte[] bytes) {
+		return ByteBuffer.wrap(bytes).order(byteOrder).getDouble();
+	}
+	public int getUShort(byte[] bytes) {
+		return ByteBuffer.wrap(bytes).order(byteOrder).getChar();
+	}
 }
