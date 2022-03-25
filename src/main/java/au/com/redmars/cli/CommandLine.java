1bedbcf11c913933cd1f6a07a914b3daa9c88c82
roman1900 <mmarschall@redmars.com.au>
2022-03-25 13:05:33 +1100

    Rename name to identifier

--- src/main/java/au/com/redmars/cli/CommandLine.java
+++ src/main/java/au/com/redmars/cli/CommandLine.java
@@ -4,11 +4,7 @@ import java.text.ParseException;
 import java.util.Optional;
 
 public class CommandLine {
-<<<<<<< HEAD
     public static void parseCommandLine(Commands commands,String args[]) throws ParseException{
-=======
-    public void parseCommandLine(Commands commands,String args[]) throws ParseException{
->>>>>>> 74c3347df4ef3afcf87bb204dfe5d90ab8299e4f
         int i = 0;
         while (i < args.length){
             if (args[i].startsWith("-")) {
