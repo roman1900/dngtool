d3c4a4eac451583fa2b50e0f94fbb13df40c35e6
roman1900 <mmarschall@redmars.com.au>
2022-03-25 12:56:27 +1100

    Add isDNG to DNG Class

--- src/main/java/au/com/redmars/DNG.java
+++ src/main/java/au/com/redmars/DNG.java
@@ -16,17 +16,21 @@ import au.com.redmars.ifd.TagIdentifier;
 import au.com.redmars.ifd.UndefinedIFD;
 
 public class DNG {
-	private ImageHeader imageHeader;
+    private ImageHeader imageHeader;
     private byte[] rawDNGBytes;
-    
-    
-    public int readIFDEntries(IFDStruct root,Integer offset) throws Exception{
+
+    public static boolean isDNG(String filePath) {
+        return filePath.toUpperCase().endsWith("DNG") || filePath.toUpperCase().endsWith("TIF");
+    }
+
+    public int readIFDEntries(IFDStruct root, Integer offset) throws Exception {
         if (offset == null)
             offset = imageHeader.ifdOffset.intValue();
         int ptr = offset + 2;
-        int ifdEntryCount = ByteBuffer.wrap(Arrays.copyOfRange(rawDNGBytes, offset, offset + 2)).order(imageHeader.byteOrder).getChar();
-        for(int i = 0; i < ifdEntryCount; ++i) {
-            IFDEntry currentTag = new IFDEntry(rawDNGBytes,ptr,imageHeader.byteOrder);
+        int ifdEntryCount = ByteBuffer.wrap(Arrays.copyOfRange(rawDNGBytes, offset, offset + 2))
+                .order(imageHeader.byteOrder).getChar();
+        for (int i = 0; i < ifdEntryCount; ++i) {
+            IFDEntry currentTag = new IFDEntry(rawDNGBytes, ptr, imageHeader.byteOrder);
             switch (currentTag.getFieldType()) {
                 case ASCII:
                     root.addChild(new AsciiIFD(currentTag));
@@ -57,37 +61,40 @@ public class DNG {
                     root.addChild(new ShortIFD(currentTag));
                     break;
                 default:
-                    throw new Exception(String.format("Missing fieldType in switch statement encountered: %d",currentTag.getFieldType()));
+                    throw new Exception(String.format("Missing fieldType in switch statement encountered: %d",
+                            currentTag.getFieldType()));
             }
-            if (currentTag.getTagIdentifier() == TagIdentifier.SubIFDs || currentTag.getTagIdentifier() == TagIdentifier.Exif_IFD) {
+            if (currentTag.getTagIdentifier() == TagIdentifier.SubIFDs
+                    || currentTag.getTagIdentifier() == TagIdentifier.Exif_IFD) {
                 (new LongIFD(currentTag)).getValues()
-                    .forEach(x -> {
-                        root.getLast().addChild(new IFDEntry());
-                        try {
-                            readIFDEntries(root.getLast().getLast(),x.intValue());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
+                        .forEach(x -> {
+                            root.getLast().addChild(new IFDEntry());
+                            try {
+                                readIFDEntries(root.getLast().getLast(), x.intValue());
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
             }
-            //TODO: Convert DNGPrivateData into an IFDStruct with IFDEntry children
+            // TODO: Convert DNGPrivateData into an IFDStruct with IFDEntry children
             // if (currentTag.getTagIdentifier() == TagIdentifier.DNGPrivateData) {
-			// 	Boolean foundnul = false;
-			// 	int o = currentTag.getOffset();
-			// 	while (!foundnul) {
-			// 		if (rawDNGBytes[o] == 0) foundnul = true;
-			// 		else o++;
-			// 	}
-			// 	System.out.printf("%d %s\r\n",currentTag.getOffset(),new String(Arrays.copyOfRange(rawDNGBytes,currentTag.getOffset(),o+1)));
-			// }
-			
+            // Boolean foundnul = false;
+            // int o = currentTag.getOffset();
+            // while (!foundnul) {
+            // if (rawDNGBytes[o] == 0) foundnul = true;
+            // else o++;
+            // }
+            // System.out.printf("%d %s\r\n",currentTag.getOffset(),new
+            // String(Arrays.copyOfRange(rawDNGBytes,currentTag.getOffset(),o+1)));
+            // }
+
             ptr = ptr + 12;
         }
-        return ByteBuffer.wrap(Arrays.copyOfRange(rawDNGBytes, ptr, ptr+4)).order(imageHeader.byteOrder).getInt();
+        return ByteBuffer.wrap(Arrays.copyOfRange(rawDNGBytes, ptr, ptr + 4)).order(imageHeader.byteOrder).getInt();
     }
 
-	DNG(byte[] rawDNGBytes) throws Exception{
+    DNG(byte[] rawDNGBytes) throws Exception {
         this.rawDNGBytes = rawDNGBytes;
         imageHeader = new ImageHeader(rawDNGBytes);
-	}
+    }
 }
