d1af0382fffb053692373fd2e9e9aa67ecd5ee9a
roman1900 <mmarschall@redmars.com.au>
2022-03-25 12:57:34 +1100

    Update tests to reflect cli package

--- src/test/java/au/com/redmars/AppTest.java
+++ src/test/java/au/com/redmars/AppTest.java
@@ -1,10 +1,16 @@
 package au.com.redmars;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.util.List;
 
 import org.junit.Test;
 
-import au.com.redmars.exceptions.IncorrectFileNameException;
+import au.com.redmars.cli.Command;
+import au.com.redmars.cli.CommandLine;
+import au.com.redmars.cli.Commands;
 
 /**
  * Unit test for simple App.
@@ -14,88 +20,132 @@ public class AppTest
     @Test
     public void noArgumentsProvided()
     {
-        String[] args = null;
-        boolean error = false;
+        String[] args = new String[0];
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IllegalArgumentException e) {
-            error = true;
-        } catch (IncorrectFileNameException e) {
-            
+            CommandLine.parseCommandLine(commands, args);
         }
-        finally {
-            assertTrue("No command line arguments provided",error);
+        catch (ParseException e) {
+            e.printStackTrace();
         }
+        List<String> files = commands.getNonCommandArgs();
+        assertTrue("No command line arguments provided",files.size()==0);    
     }
     @Test
     public void wrongFileExtensionProvided()
     {
         String[] args=new String[1];
         args[0] = "img-2345.jpg";
-        boolean wrongExt = false;
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IncorrectFileNameException e) {
-            wrongExt = true;
-        } finally {
-            assertTrue("Incorrect Filename supplied",wrongExt);
+            CommandLine.parseCommandLine(commands, args);
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<String> files = commands.getNonCommandArgs();
+
+        if (files.size() < 1)
+        {
+            throw new IllegalArgumentException("No arguments provided");
+        }
+        
+        files.forEach(f -> assertFalse("Wrong file extension",DNG.isDNG(f)) );
     }
     @Test
     public void LowerDNGFileExtensionProvided()
     {
         String[] args=new String[1];
         args[0] = "img-2345.dng";
-        boolean wrongExt = false;
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IncorrectFileNameException e) {
-            wrongExt = true;
-        } finally {
-            assertTrue("Incorrect Filename supplied",!wrongExt);
+            CommandLine.parseCommandLine(commands, args);
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<String> files = commands.getNonCommandArgs();
+
+        if (files.size() < 1)
+        {
+            throw new IllegalArgumentException("No arguments provided");
+        }
+        files.forEach(f -> assertTrue("Lowercase dng Filename supplied",DNG.isDNG(f)));
     }
     @Test
     public void UpperDNGFileExtensionProvided()
     {
         String[] args=new String[1];
         args[0] = "img-2345.DNG";
-        boolean wrongExt = false;
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IncorrectFileNameException e) {
-            wrongExt = true;
-        } finally {
-            assertTrue("Incorrect Filename supplied",!wrongExt);
+            CommandLine.parseCommandLine(commands, args);
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<String> files = commands.getNonCommandArgs();
+
+        if (files.size() < 1)
+        {
+            throw new IllegalArgumentException("No arguments provided");
+        }
+        files.forEach(f ->
+        assertTrue("Uppercase DNG Filename supplied",DNG.isDNG(f)));
     }
     @Test
     public void LowerTIFFileExtensionProvided()
     {
         String[] args=new String[1];
         args[0] = "img-2345.tif";
-        boolean wrongExt = false;
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IncorrectFileNameException e) {
-            wrongExt = true;
-        } finally {
-            assertTrue("Incorrect Filename supplied",!wrongExt);
+            CommandLine.parseCommandLine(commands, args);
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<String> files = commands.getNonCommandArgs();
+
+        if (files.size() < 1)
+        {
+            throw new IllegalArgumentException("No arguments provided");
+        }
+        files.forEach(f ->
+        assertTrue("Lowercase tif Filename supplied",DNG.isDNG(f)));
     }
     @Test
     public void UpperTIFFileExtensionProvided()
     {
         String[] args=new String[1];
         args[0] = "img-2345.TIF";
-        boolean wrongExt = false;
+        Command command = new Command("r","Create a report");
+        Commands commands = new Commands();
+        commands.addCommand(command);
         try {
-            App.main(args);
-        } catch (IncorrectFileNameException e) {
-            wrongExt = true;
-        } finally {
-            assertTrue("Incorrect Filename supplied",!wrongExt);
+            CommandLine.parseCommandLine(commands, args);
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<String> files = commands.getNonCommandArgs();
+
+        if (files.size() < 1)
+        {
+            throw new IllegalArgumentException("No arguments provided");
+        }
+        files.forEach(f ->
+        assertTrue("Uppercase TIF Filename supplied",DNG.isDNG(f)));
     }
 
 }
